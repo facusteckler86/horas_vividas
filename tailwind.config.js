ae2a795880dd3fe2aadc215a2a613b4575acae91
facusteckler86 <facundomsteckler@gmail.com>
2024-10-29 18:07:43 -0300

    instalo tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/**ver como tengo que instalar tailwind */
